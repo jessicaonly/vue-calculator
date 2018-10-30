4b82746923360ca1006f8f6057a0d70f8bbfe5d2
Jessica Skok <jessica.skok@gmail.com>
2018-10-29 23:45:56 -0700

    finish lil calculator!

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,38 +1,96 @@
 <template>
   <div class="calculator">
-    <div class="display">0.012031</div>
-    <div class="btn">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn operator">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div class="display">{{current || 0}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div @click="divide" class="btn operator">/</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="times" class="btn operator">x</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="minus" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="add" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
+    <div @click="equal" class="btn">=</div>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      previous: null,
+      operatorClicked: false,
+      current: '600',
+      operator: null,
+    }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    },
+    sign() {
+      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : -`${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number){
+      if (this.operatorClicked){
+        this.current = '';
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot(){
+      if (this.current.indexOf('.') === -1){
+        this.append('.');
+      }
+    },
 
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide(){
+      this.operator = (a, b) => a/b;
+      this.setPrevious();
+    },
+    times(){
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus(){
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add(){
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal(){
+      this.current = `${this.operator(parseFloat(this.current), parseFloat(this.previous))}`;
+      this.previous = null;
+    }
   }
+  }
+  
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .calculator {
     display: grid;
+    width: 400px;
+    margin: 0 auto;
     font-size: 40px;
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: minmax(50px, auto);
@@ -40,7 +98,7 @@ export default {
 
   .display {
     grid-column: 1 / 5;
-    background-color: red;
+    background-color: grey;
   }
 
   .zero {
@@ -48,8 +106,8 @@ export default {
   }
   
   .btn {
-    background-color: #eee;
-    border: 1px solid #333;
+    background-color: lightsteelblue;
+    border: 1px solid #999;
   }
 
   .operator {
